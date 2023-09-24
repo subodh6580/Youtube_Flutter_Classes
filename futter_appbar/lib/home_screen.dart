38e4696f3fc0309c446db77e4bd176435ae720c6
Subodh Kumar <subodh.kumar@tcsens.com>
2023-09-24 13:36:44 +0800

    Flutter Appbar Widget Added

---
+++ futter_appbar/lib/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text('Home Screen'),
+        leading: Icon(Icons.arrow_back_ios),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.abc),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar')));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar')));
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'My First Application',
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
+    );
+  }
+}
