3db22ab423a0828eb968ea89f302e240e0e70d90
Subodh Kumar <subodh.kumar@tcsens.com>
2024-01-10 22:59:44 +0800

    Drawer menu added in HomePageScreen 10-JAN-24

--- my_first_app/lib/homepage.dart
+++ my_first_app/lib/homepage.dart
@@ -8,6 +8,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,16 +42,48 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('HomePage: Engineer Malaysia Wala'),
       ),
       body: Center(
-          child: Column(
-        children: [
-          //Image.network('https://picsum.photos/250?image=9'),
-          const Image(image: AssetImage('assets/logo/logo.JPG')),
-          const Text(
-            'Breaking News',
-            style: TextStyle(fontSize: 50),
-          ),
-        ],
-      )),
+        child: _widgetOptions[_selectedIndex],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('User Profile Image'),
+            ),
+            ListTile(
+              selectedTileColor: Colors.red,
+              title: const Text('Home'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                _onItemTapped(0);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              selectedTileColor: Colors.red,
+              title: const Text('Business'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                _onItemTapped(1);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              selectedTileColor: Colors.red,
+              title: const Text('School'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                _onItemTapped(2);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ my_first_app/lib/profile_screen_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+//import 'package:my_first_app/homepage.dart';
+import 'package:my_first_app/login_screen_page.dart';
+
+class ProfileScreenPage extends StatefulWidget {
+  @override
+  State<ProfileScreenPage> createState() => _ProfileScreenPageState();
+}
+
+class _ProfileScreenPageState extends State<ProfileScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Profile Screen Page'),
+    );
+  }
+}
